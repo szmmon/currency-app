dbe9d4d00f3a00eb9cf6a42f41cf80503cbb2229
Szymon Ziemianek <szymonziemianek@gmail.com>
2023-08-02 08:20:02 +0200

    finished css, started js

---
+++ script.js
@@ -0,0 +1,6 @@
+const fromCur = document.querySelector(".from select");
+const toCur = document.querySelector(".to select");
+const getbtn = document.querySelector("form button");
+const exicon = document.querySelector("form .reverse");
+const amount = document.querySelector("form input");
+const exRateTxt = document.querySelector("form .result");
